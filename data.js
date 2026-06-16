508f5fd1d0723f92f49ac0a0d929107c48534a6b
EagleAngel <angeljimenez422@gmail.com>
2026-06-15 22:17:23 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const participantsData = [
+    { name: "José Primo", teams: ["Ghana", "Argelia", "Inglaterra", "Uruguay"] },
+    { name: "Tío Pepe", teams: ["Costa de Marfil", "Senegal", "Suecia", "Argentina"] },
+    { name: "Maestro Asel", teams: ["Curazao", "Croacia", "Brasil", "Arabia Saudita"] },
+    { name: "Marialaura", teams: ["Sudáfrica", "Noruega", "Nueva Zelanda", "Irán"] },
+    { name: "Ángel", teams: ["España", "Australia", "Túnez", "Cabo Verde"] },
+    { name: "Laura", teams: ["Bosnia y Herzegovina", "Ecuador", "Irak", "Suiza"] },
+    { name: "Vane", teams: ["Corea del Sur", "Portugal", "RD del Congo", "Colombia"] },
+    { name: "Andrea", teams: ["Egipto", "Canadá", "Austria", "México"] },
+    { name: "Sofi", teams: ["Jordania", "Uzbekistán", "Panamá", "Escocia"] },
+    { name: "Meche", teams: ["Bélgica", "Haití", "Paraguay", "Estados Unidos"] },
+    { name: "Uriel", teams: ["Turquía", "Alemania", "Qatar", "Marruecos"] },
+    { name: "Karla", teams: ["Francia", "Chequia", "Japón", "Países Bajos"] }
+];
